edb54fb3c3ef6251ee196c3d9482dabcfbdd554c
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2020-11-19 10:48:03 +0500

    Modified the homepage, added logo on top in the menu bar.:

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -20,7 +20,7 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
             <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top">Adventures</a>
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">Adventures <small style="font-size: 10px;">Explore nature with us</small></a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars"></i>
@@ -163,7 +163,7 @@
             </div>
         </section>
         <!-- Footer-->
-        <footer class="footer bg-black small text-center text-white-50"><div class="container">Copyright © Your Website 2020</div></footer>
+        <footer class="footer bg-black small text-center text-white-50"><div class="container">Copyright © Adventures <?php echo date('Y'); ?>. All rights reserved.</div></footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
